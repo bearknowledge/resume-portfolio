4b3ad794bd994f0e37f86e307602e15163bcfd8b
Zach Miller <home@Zachs-MBP.hsd1.ga.comcast.net>
2021-10-11 21:42:40 -0400

    updated entire site

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,6 @@
 
 (function($) {
 
-
 	var	$window = $(window),
 		$body = $('body'),
 		$wrapper = $('#wrapper'),
@@ -69,21 +68,6 @@
 	// Main.
 		var	delay = 325,
 			locked = false;
-
-			var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000);
-}
 
 		// Methods.
 			$main._show = function(id, initial) {
